46d77f6385ce71cd12e13e7fa0148d17f2d3567a
codr7 <andreas.gone.wild@gmail.com>
2019-08-14 18:57:48 +0200

    improved reader

--- src/calcl/read.cpp
+++ src/calcl/read.cpp
@@ -1,51 +1,52 @@
 #include "cidk/cx.hpp"
 #include "cidk/ops/push.hpp"
-#include "cidk/ops/call.hpp"
+#include "cidk/ops/call_bin.hpp"
 #include "cidk/read.hpp"
 
 #include "calcl/read.hpp"
 
 namespace calcl {
-  struct ReadCx {
-    const Sym *op;
-    ValType *lhs_type;
-
-    ReadCx(): op(nullptr), lhs_type(nullptr) {};
-  };
-
-  void read_next(Cx &cx, Pos &pos, ReadCx &rcx, istream &in, Ops &out) {
+  optional<Val> read_one(Cx &cx, Pos &pos, istream &in) {
     cidk::skip_ws(pos, in);
     
     if (char c(0); in.get(c)) {
-      in.unget();
-      
-      if (isdigit(c)) {
-        Val v(read_num(cx, pos, in));
-        out.emplace_back(cx, pos, ops::Push, v);
-
-        if (rcx.lhs_type && rcx.op) {
-          ValType *vt(v.type);
-          
-          auto fid((vt == rcx.lhs_type)
-                   ? rcx.op
-                   : cx.intern(str(rcx.op, '[', vt->id, ']')));
-          
-          out.emplace_back(cx, pos, ops::Call, rcx.lhs_type->env.get(pos, fid));
-          rcx.op = nullptr;
-          rcx.lhs_type = nullptr;
-        } else {
-          rcx.lhs_type = v.type;
-        }
-      } else if (isgraph(c)) {
-        rcx.op = read_id(cx, pos, in).as_sym;
+      if (c == '-') {
+        char c1(in.get());
+        in.unget();
+        in.unget();
+        if (isdigit(c1)) { c = c1; }
       } else {
-        throw ESys(pos, "Invalid input: ", c);
+        in.unget();
       }
+      
+      if (isdigit(c)) { return read_num(cx, pos, in); }
+      if (isgraph(c)) { return read_id(cx, pos, in); }
+      throw ESys(pos, "Invalid input: ", c);
     }
+
+    return {};
+  }
+
+  bool read_three(Cx &cx, Pos &pos, istream &in, Ops &out) {
+    auto x(read_one(cx, pos, in));
+    if (!x) { return false; }
+    out.emplace_back(cx, pos, ops::Push, *x);
+
+    Pos vp(pos);
+    auto op(read_one(cx, pos, in));
+    if (!op) { return false; }
+    if (op->type != &cx.sym_type) { throw ESys(vp, "Invalid operation: ", *op); }
+    
+    vp = pos;
+    auto y(read_one(cx, pos, in));
+    if (!y) { throw ESys(vp, "Missing right operand"); }
+    out.emplace_back(cx, pos, ops::Push, *y);
+
+    out.emplace_back(cx, pos, ops::CallBin, *op);
+    return true;
   }
 
   void read(Cx &cx, Pos &pos, istream &in, Ops &out) {
-    ReadCx rcx;
-    while (!in.eof()) { read_next(cx, pos, rcx, in, out); }
+    while (read_three(cx, pos, in, out));
   }
 }
