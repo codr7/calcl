92419c70b65a8884e447c17ff1a670a391aff7eb
codr7 <andreas.gone.wild@gmail.com>
2019-08-26 23:37:28 +0200

    added libs

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 #include "cidk/cx.hpp"
 #include "cidk/ext_id.hpp"
+#include "cidk/libs/abc.hpp"
+#include "cidk/libs/math.hpp"
 
 #include "calcl/calcl.hpp"
 #include "calcl/plugin.hpp"
@@ -17,6 +19,12 @@ enum struct Mode {nil, load, repl};
 
 int main(int argc, char *argv[]) {   
   Cx cx;
+  Lib &abc(libs::init_abc(cx, Pos::_));
+  cx.env.use(cx, Pos::_, abc.env);
+
+  Lib &math(libs::init_math(cx, Pos::_));
+  cx.env.use(cx, Pos::_, math.env);
+  
   cidk_pluginit(cx);
   Mode m(Mode::nil);
 
